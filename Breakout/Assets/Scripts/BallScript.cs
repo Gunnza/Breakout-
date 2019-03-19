c22641bab8e13df5d3936b79714a1dc17edc9e6a
Christopher McGovern <chrismcg_@hotmail.com>
2019-03-19 00:39:12 +0000

    Cleaner code

--- Breakout/Assets/Scripts/BallScript.cs
+++ Breakout/Assets/Scripts/BallScript.cs
@@ -3,22 +3,19 @@ using System.Collections;
 
 public class BallScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
+	//Destroy the ball and respawn a new ball on the paddle 
 	public void Die() {
-	
+		
+		//destroy ball gameobject 
 		Destroy (gameObject);
+		
+		//Find the player paddle object
 		GameObject paddleObject = GameObject.Find("Paddle");
+		
+		//Get the script Component from the paddle gameobject
 		PaddleScript paddleScript = paddleObject.GetComponent<PaddleScript>();
+		
+		//Call the paddlescript respawn function
 		paddleScript.Spawnball();
 	}
 }
