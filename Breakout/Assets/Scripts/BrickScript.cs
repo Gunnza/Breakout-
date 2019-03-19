aa881aa326b82d1153cb37b136de39394b0c8a08
Christopher McGovern <chrismcg_@hotmail.com>
2019-03-19 00:54:58 +0000

    Cleaner Code

--- Breakout/Assets/Scripts/BrickScript.cs
+++ Breakout/Assets/Scripts/BrickScript.cs
@@ -2,27 +2,37 @@ using UnityEngine;
 using System.Collections;
 
 public class BrickScript : MonoBehaviour {
-	
-	static int numBricks = 0;
-	public int pointValue = 1;
-	public int hitPoints = 1;
 
+	//Int variable to know how many bricks their are
+	static int numBricks = 0;
+	
+	//Value of each point
+	public int pointValue = 1;
+	
+	//Bricks health integer
+	public int hitPoints = 1;
+	
+	//GUI variable 
 	GUIText guivictory;
 
-	// Use this for initialization
+	// Every brick adds 1 to number of bricks
 	void Start () {
-	numBricks++;
+		numBricks++;
 	}
 		
-	// Update is called once per frame
 	void Update () {
-	guivictory = GameObject.Find("guivictory").GetComponent<GUIText>();
-	guivictory.enabled = false; 
+		//Getting the text component on the gui game object 
+		guivictory = GameObject.Find("guivictory").GetComponent<GUIText>();
+		guivictory.enabled = false; 
 	}
 	
+	//function that recognises when object collides 
 	void OnCollisionEnter( Collision col) {
+		
+		//reduce health of the brick
 		hitPoints--;
 		
+		//call die function if health is zero 
 		if (hitPoints <= 0 ) 
 		{
 			Die();
@@ -31,17 +41,27 @@ public class BrickScript : MonoBehaviour {
 		
 	void Die() {
 		
+		//delete the gameobject from the scene
 		Destroy (gameObject);
+		
+		//Get the players paddle's script component PaddleScript and add a point equal to the point value
 		GameObject.Find("Paddle").GetComponent<PaddleScript>().AddPoint(pointValue);
+		
+		//reduce the number of bricks in the game
 		numBricks--;
+		
+		//console logging the number of bricks in the scene
 		Debug.Log (numBricks);
+		
+		//victory condition if the number of bricks is zero
 		if (numBricks <= 0) {
-				//Load a new level
+			
+			//Stop time in the game	
 			Time.timeScale = 0;
+			
+			//enable the victory GUI object
 			guivictory.enabled = true;
-			
-			
-			
+				
 		}
 	}
 }
